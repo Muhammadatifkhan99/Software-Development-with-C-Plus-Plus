06118b428f519b7aa179a38871a3a32c4e246cce
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-10-22 12:39:08 +0500

    static libraries in C++

---
+++ StaticLibraries/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#include"add.h"
+#include"multiply.h"
+
+
+int main(){
+	auto sum = add(10,20);
+	std::cout<<"Sum: "<<sum<<'\n';
+
+	auto product = multiply(10,20);
+	std::cout << "Product: "<<product<<'\n';
+
+
+	return 0;
+}
