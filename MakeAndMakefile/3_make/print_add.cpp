92231797de64e930ff8004f11b81214dca0ff132
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2025-11-21 20:10:25 +0500

    make and makefile for C++

---
+++ MakeAndMakefile/3_make/print_add.cpp
@@ -0,0 +1,11 @@
+// A simple program that prints the sum of two numbers
+// By: Nick from CoffeeBeforeArch
+
+#include <iostream>
+
+#include "add.h"
+
+int main() {
+  std::cout << add(10, 20) << '\n';
+  return 0;
+}
